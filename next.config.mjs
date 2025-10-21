2f7a1a45b95b6c6b0314841805347575310358af
m-junaid0035 <muhammadjunaid0035@gmail.com>
2025-10-21 07:03:04 -0700

    resolve eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -5,5 +9,4 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
 };
-
-module.exports = nextConfig;
+export default withNextIntl(nextConfig);
